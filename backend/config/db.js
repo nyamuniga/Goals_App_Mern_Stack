d9322908ad5408fd7bf233f59aa42538e37dce1c
nyamuniga <57840315+nyamuniga@users.noreply.github.com>
2023-11-01 13:50:22 +0200

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline)
+    await mongoose.connect(process.env.MONGO_URI);
+        // console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline)
         
     } catch (error) {
         console.log(error)
